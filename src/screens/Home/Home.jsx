ea3a68c9b9ee608e2da37ead3b8cd340b24023a1
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-12-12 20:20:24 +0200

    minor changes

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
-import logo from '../../assets/img/home/Logo.png';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
